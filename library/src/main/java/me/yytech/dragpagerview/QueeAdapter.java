b5029d631cf828e3c77bfb65c8c903a83883ef93
eyyoung <yy910226>
2015-06-14 15:05:45 +0800

    DragPagerView

---
+++ library/src/main/java/me/yytech/dragpagerview/QueeAdapter.java
@@ -0,0 +1,14 @@
+package me.yytech.dragpagerview;
+
+import android.view.View;
+
+/**
+ * Created by Young on 2015/6/14.
+ */
+public interface QueeAdapter {
+
+    View getNewView();
+
+    int getCount();
+
+}
